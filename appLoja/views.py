c26eb2428caf0cd14efaaebabe7c57baeeafb80c
leondvieira <leonardovieirabfh@gmail.com>
2024-09-24 17:43:55 -0300

    FEAT: validação de quantidade em estoque

--- appLoja/views.py
+++ appLoja/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 
 from appLoja.models import Produto, Categoria, Venda
@@ -44,3 +44,16 @@ class VendaViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        id_produto = serializer.data.get("produto")
+        produto = Produto.objects.get(id=id_produto)
+        if produto.unidade < serializer.data.get("quantidade"):
+            return Response({"details": "Quantidade não disponível no estoque."}, status.HTTP_403_FORBIDDEN)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
