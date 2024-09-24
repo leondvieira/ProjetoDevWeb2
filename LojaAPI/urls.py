dea6a177a0b1a1e3f03af6c1730013e3dd4c9872
leondvieira <leonardovieirabfh@gmail.com>
2024-09-23 21:10:27 -0300

    Feat: Implementando Swagger

--- LojaAPI/urls.py
+++ LojaAPI/urls.py
@@ -16,9 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework import routers
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from appLoja import views
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Loja API",
+      default_version='v1',
+      description="Documentação para utilização da Loja API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'produtos', views.ProdutoViewSet)
@@ -29,4 +49,7 @@ urlpatterns = [
     path('', include(router.urls),),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
